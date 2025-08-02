9651e822a083ce472627471f3bd7bcd54c30d537
Duckmyster <Duckmyster12@gmail.com>
2025-08-02 13:28:01 +0800

    add two new .txt files to books, finish bookbot

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import sys
 from stats import get_word_count
 from stats import char_count
+from stats import dictionary_sort
+
+
 
 def get_book_text(filepath):
     with open(filepath) as text:
@@ -10,9 +14,25 @@ def get_book_text(filepath):
 
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    filepath = sys.argv[1]
+    text = get_book_text(filepath)
     word_count = get_word_count(text)
-    print(f"{word_count} words found in the document")
-    print(char_count(text))
+    character_count = char_count(text)
+    sorted_dictionary = dictionary_sort(character_count)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for item in sorted_dictionary:
+        if str.isalpha(item["char"]):
+            print(f"{item["char"]}: {item["num"]}")
+    print("============= END ===============")
+
 
 main()
--- stats.py
+++ stats.py
@@ -14,8 +14,20 @@ def char_count(text):
                 letter_count[letter] += 1
             else:
                 letter_count[letter] = 1
-    return letter_count                
+    return letter_count     
+#counts total letters from list of strings           
 
 def dictionary_sort(dictionary):
+    def sort_on(item):
+        return item["num"]
+    #defines what part to sort by within each dictionary in the list
+    
+    char_dict_list = []
     for character in dictionary:
-        print(character) 
+        char_dict = {
+            "char": character, 
+            "num": dictionary[character]}
+        char_dict_list.append(char_dict)
+
+    char_dict_list.sort(reverse=True, key=sort_on)
+    return char_dict_list
